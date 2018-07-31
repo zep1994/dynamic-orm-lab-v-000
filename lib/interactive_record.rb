e40d7d25afab7eaecb66b7a588cb036ff34a439b
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-07-31 20:04:44 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,6 +26,10 @@ class InteractiveRecord
     end
   end
 
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  end
+
   def table_name_for_insert
       self.class.table_name
   end
