0d154a6649429dbd400914b0222071e396ef1d9c
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-07-31 19:40:13 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,21 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+   self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+   DB[:conn].results_as_hash = true
+
+   sql = "pragma table_info('#{table_name}')"
+
+   table_info = DB[:conn].execute(sql)
+   column_names = []
+   table_info.each do |row|
+     column_names << row["name"]
+   end
+   column_names.compact
+  end
+
 end
