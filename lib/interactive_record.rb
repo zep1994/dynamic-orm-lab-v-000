111bbc0e31d2d3d3bb24296351ea4f3736c93c09
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-07-31 19:47:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,4 +20,9 @@ class InteractiveRecord
    column_names.compact
   end
 
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
 end
